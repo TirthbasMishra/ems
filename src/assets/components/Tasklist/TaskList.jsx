1e8b429c21216d6d4f670c78c3865040db51de02
Tirthbas Mishra <99424426+TirthbasMishra@users.noreply.github.com>
2024-10-28 03:09:29 +0530

    Employee Dashboard Completed

---
+++ src/assets/components/Tasklist/TaskList.jsx
@@ -0,0 +1,55 @@
+import React from 'react'
+
+const TaskList = () => {
+  return (
+    <div id='tasklist' className='h-[55%] overflow-x-auto flex items-center justify-start gap-5 flex-nowrap w-full py-5 mt-10'>
+
+      <div className='h-full flex-shrink-0 w-[400px] p-5 bg-red-400 rounded-xl'>
+            <div className='flex justify-between  items-center'>
+            <h3 className='bg-red-600 px-3 py-1 rounded text-sm'>High</h3>
+            <h4 className='text-sm'>30 OCT 2024</h4>
+            </div>
+            <h2 className='mt-5 text-2xl font-semibold'>Make A PortFolio Website</h2>
+            <p className='text-sm mt-2'>Pariatur aute consectetur aliqua id irure labore esse ipsum ullamco. Aliqua officia adipisicing commodo cillum exercitation dolor nostrud aliqua culpa occaecat labore aute. Laboris culpa enim commodo laborum sunt ullamco sunt. Qui anim occaecat elit labore ullamco aliquip ipsum ipsum.</p>
+      </div>
+
+      <div className='h-full flex-shrink-0 w-[400px] p-5 bg-blue-400 rounded-xl'>
+            <div className='flex justify-between items-center'>
+            <h3 className='bg-red-600 px-3 py-1 rounded text-sm'>High</h3>
+            <h4 className='text-sm'>30 OCT 2024</h4>
+            </div>
+            <h2 className='mt-5 text-2xl font-semibold'>Make A PortFolio Website</h2>
+            <p className='text-sm mt-2'>Pariatur aute consectetur aliqua id irure labore esse ipsum ullamco. Aliqua officia adipisicing commodo cillum exercitation dolor nostrud aliqua culpa occaecat labore aute. Laboris culpa enim commodo laborum sunt ullamco sunt. Qui anim occaecat elit labore ullamco aliquip ipsum ipsum.</p>
+      </div>
+
+      <div className='h-full flex-shrink-0 w-[400px] p-5 bg-green-400 rounded-xl'>
+            <div className='flex justify-between  items-center'>
+            <h3 className='bg-red-600 px-3 py-1 rounded text-sm'>High</h3>
+            <h4 className='text-sm'>30 OCT 2024</h4>
+            </div>
+            <h2 className='mt-5 text-2xl font-semibold'>Make A PortFolio Website</h2>
+            <p className='text-sm mt-2'>Pariatur aute consectetur aliqua id irure labore esse ipsum ullamco. Aliqua officia adipisicing commodo cillum exercitation dolor nostrud aliqua culpa occaecat labore aute. Laboris culpa enim commodo laborum sunt ullamco sunt. Qui anim occaecat elit labore ullamco aliquip ipsum ipsum.</p>
+      </div>
+
+      <div className='h-full flex-shrink-0 w-[400px] p-5 bg-yellow-400 rounded-xl'>
+            <div className='flex justify-between  items-center'>
+            <h3 className='bg-red-600 px-3 py-1 rounded text-sm'>High</h3>
+            <h4 className='text-sm'>30 OCT 2024</h4>
+            </div>
+            <h2 className='mt-5 text-2xl font-semibold'>Make A PortFolio Website</h2>
+            <p className='text-sm mt-2'>Pariatur aute consectetur aliqua id irure labore esse ipsum ullamco. Aliqua officia adipisicing commodo cillum exercitation dolor nostrud aliqua culpa occaecat labore aute. Laboris culpa enim commodo laborum sunt ullamco sunt. Qui anim occaecat elit labore ullamco aliquip ipsum ipsum.</p>
+      </div>
+
+      <div className='h-full flex-shrink-0 w-[400px] p-5 bg-emerald-400 rounded-xl'>
+            <div className='flex justify-between  items-center'>
+            <h3 className='bg-red-600 px-3 py-1 rounded text-sm'>High</h3>
+            <h4 className='text-sm'>30 OCT 2024</h4>
+            </div>
+            <h2 className='mt-5 text-2xl font-semibold'>Make A PortFolio Website</h2>
+            <p className='text-sm mt-2'>Pariatur aute consectetur aliqua id irure labore esse ipsum ullamco. Aliqua officia adipisicing commodo cillum exercitation dolor nostrud aliqua culpa occaecat labore aute. Laboris culpa enim commodo laborum sunt ullamco sunt. Qui anim occaecat elit labore ullamco aliquip ipsum ipsum.</p>
+      </div>
+    </div>
+  )
+}
+
+export default TaskList
--- src/assets/components/other/TaskListNumbers.jsx
+++ src/assets/components/other/TaskListNumbers.jsx
@@ -2,11 +2,25 @@ import React from 'react'
 
 const TaskListNumbers = () => {
   return (
-    <div className='flex mt-10 gap-5 h-screen w-screen justify-between'>
-    <div className='h-40 w[45%] bg-red-400'>
-    <h2 >0</h2>
-    <h3>New Task</h3>
+    <div className='flex mt-10 gap-5 screen justify-between'>
+    <div className='rounded-xl w-[45%] py-6 px-9 bg-blue-400'>
+      <h2 className='text-3xl font-semibold'>1</h2>
+      <h3 className='text-xl font-medium'>New Task</h3>
+    </div>
 
+    <div className='rounded-xl w-[45%] py-6 px-9 bg-green-400'>
+      <h2 className='text-3xl font-semibold'>3</h2>
+      <h3 className='text-xl font-medium'>Completed Task</h3>
+    </div>
+
+    <div className='rounded-xl w-[45%] py-6 px-9 bg-yellow-400 text-black'>
+      <h2 className='text-3xl font-semibold'>0</h2>
+      <h3 className='text-xl font-medium'>Accepted Task</h3>
+    </div>
+
+    <div className='rounded-xl w-[45%] py-6 px-9 bg-red-400'>
+      <h2 className='text-3xl font-semibold'>0</h2>
+      <h3 className='text-xl font-medium'>Failed Task</h3>
     </div>
 
     </div>
