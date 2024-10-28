4920c7db9aaa5c9cb48eacb00ca5b923f55f0f4d
Tirthbas Mishra <99424426+TirthbasMishra@users.noreply.github.com>
2024-10-29 01:48:18 +0530

    setting up who is logged in local Storage

--- src/App.jsx
+++ src/App.jsx
@@ -7,33 +7,43 @@ import { AuthContext } from './context/AuthProvider'
 
 const App = () => {
 
- const [user, setUser] = useState(null)
- const authData = useContext(AuthContext)
- 
- const handleLogin = (email,password) =>{
-    if(email == 'admin@me.com' && password == '123')
-    {
+  const [user, setUser] = useState(null)
+  const authData = useContext(AuthContext)
+
+  useEffect(() => {
+    if(authData){
+      const loggedInUser = localStorage.getItem("loggedInUser")
+      if(loggedInUser){
+        setUser(loggedInUser.role)
+      }
+    }
+  }, [authData])
+  
+
+  const handleLogin = (email, password) => {
+    if (email == 'admin@me.com' && password == '123') {
       setUser('admin')
+      localStorage.setItem('loggedInUser',JSON.stringify({role:'admin'}))
     }
-    else if (email == 'user@me.com' && password == '123')
-    {
+    else if (authData && authData.employees.find((e) => email == e.email && e.password == password)) {
       setUser('employee')
+      localStorage.setItem('loggedInUser',JSON.stringify({role:'employee'}))
     }
-    else{
+    else {
       alert("Invalid credentials")
     }
- }
+  }
 
 
 
 
   return (
-   <>
-      {!user ? <Login handleLogin={handleLogin} /> :''}
-      {user == 'admin' ? <AdminDashboard /> : <EmployeeDashboard /> }
+    <>
+      {!user ? <Login handleLogin={handleLogin} /> : ''}
+      {user == 'admin' ? <AdminDashboard /> : <EmployeeDashboard />}
       {/*  */}
       {/*  */}
-   </>
+    </>
   )
 }
 
