ff54af5e26ff8e7fd8163c71a20a461d2ee19f38
Tirthbas Mishra <99424426+TirthbasMishra@users.noreply.github.com>
2024-10-31 02:44:17 +0530

    Create Task feature added

--- src/context/AuthProvider.jsx
+++ src/context/AuthProvider.jsx
@@ -1,34 +1,24 @@
 import React, { createContext, useEffect, useState } from 'react'
-import { getLocalStorage, setLocalStorage } from '../utils/LocalStorage'
+import { getLocalStroage } from '../utils/LocalStrorage'
+
 export const AuthContext = createContext()
 
+const AuthProvider = ({children}) => {
 
+  const[userData,setUserData] = useState(null)
 
+  useEffect(()=>{
+    const {employees} = getLocalStroage()
+    setUserData(employees)
+  },[])
 
-const AuthProvider = ({ children }) => {
-   
-  
-
-   const [userData, setUserData] = useState(null)
-
-   useEffect(() => {
-
-      setLocalStorage()
-      const { employees, admin } = getLocalStorage()
-      setUserData({ employees, admin })
-
-
-   },[])
-
-
-
-   return (
-      <div>
-         <AuthContext.Provider value={userData}>
-            {children}
-         </AuthContext.Provider>
-      </div>
-   )
+  return (
+    <div>
+      <AuthContext.Provider value={[userData, setUserData]}>
+        {children}
+      </AuthContext.Provider>
+    </div>
+  )
 }
 
 export default AuthProvider
